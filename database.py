7b445273b5ef3e07c65f55ab7a655007be0fe01a
gg <gg@weareway.com>
2024-05-16 00:53:26 +0300

    add login

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ import psycopg2
 from utils import dict_factory
 import os
 from dotenv import load_dotenv
+from psycopg2.extras import RealDictCursor
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
@@ -12,7 +13,8 @@ conn = psycopg2.connect(
     database = os.getenv('DB'),
     user = os.getenv('USER_DB'),
     password = os.getenv('PASS_DB'),
-    host = os.getenv('HOST_DB')
+    host = os.getenv('HOST_DB'),
+
 )
 
 def init_tables():
@@ -23,7 +25,8 @@ def init_tables():
                 id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
                 name VARCHAR NOT NULL,
                 age INTEGER NOT NULL,
-                gender VARCHAR NOT NULL
+                gender VARCHAR NOT NULL,
+                password varchar NOT NULL
             );
             CREATE TABLE 
                 IF NOT EXISTS Goals(
@@ -53,6 +56,18 @@ def init_tables():
 
 def new_user(conn):
     cur = conn.cursor()
+
+
+def valedate_user(name,password):
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+    cur.execute(f"""SELECT * FROM Users where name='{name}' """)
+    user = cur.fetchone()
+    print(user['password'])
+    if user and user['password'] == password:
+        return {'id': user['id'], 'name':user['name'], 'success':True}
+    else:
+        return {'success':False}
+
 
 def get_user_info ():
     cur = conn.cursor()
--- main.py
+++ main.py
@@ -1,17 +1,31 @@
-from flask import Flask, render_template, make_response
+from flask import Flask, request, render_template, make_response,jsonify
 from flask_restful import Api, Resource
 from flask_cors import CORS, cross_origin
-from database import get_user_info
+from database import *
 
 app = Flask(__name__)
-@app.route('/main', methods=['GET'])
-@cross_origin(origin='https://04b7-178-178-92-202.ngrok-free.app', headers=['Content- Type', 'Authorization'])
-def get():
-    return get_user_info(), 200
-     
 app.config['CORS_HEADERS'] = 'Content-Type'
 cors = CORS(app, resources={r"/": {"origins": "https://04b7-178-178-92-202.ngrok-free.app"}})
 api = Api(app)
+@app.route('/main', methods=['GET'])
+@cross_origin(origin='https://04b7-178-178-92-202.ngrok-free.app', headers=['Content-Type', 'Authorization'])
+def get():
+    return get_user_info(), 200
+
+@app.route('/login', methods=['POST'])
+@cross_origin(origin='https://04b7-178-178-92-202.ngrok-free.app', headers=['Content-Type', 'Authorization'])
+def login():
+    print(request.json)
+    name = request.json['name']
+    password = request.json['password']
+    response = jsonify( valedate_user(name,password))
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    response.headers['Access-Control-Allow-Methods'] = 'POST'
+    return response, 200
+
+     
+
 
 if __name__ == '__main__':
     app.run(debug=True)
